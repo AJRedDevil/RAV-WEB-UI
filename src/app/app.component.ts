bac704491979f30868f8dcca5844e5c799fae70f
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2016-11-12 00:46:39 +0545

    Logging rav servicr url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
     constructor() {
+        console.log(process.env.RAV_SERVICE);
         var RAV_SERVICE_URL = process.env.RAV_SERVICE || "http://localhost:8080/ravnepal";
         console.log(RAV_SERVICE_URL);
         localStorage.setItem('baseUrl', RAV_SERVICE_URL);
