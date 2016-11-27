097c4817a58940447c083f8bec97e94d583bfff4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2016-11-27 21:09:55 +0545

    Made the change solution required for toastr for angular v2.2.x

--- src/app/home/main.component.ts
+++ src/app/home/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener ,OnInit, ViewChild } from "@angular/core";
+import { Component, HostListener ,OnInit, ViewChild, ViewContainerRef } from "@angular/core";
 import { Observable } from 'rxjs/Rx';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
 
@@ -23,13 +24,16 @@ private _timeout = 5*60; // 5 minutes
     // We'll need to include a reference to our authService in the constructor to gain access to the API's in the view
     constructor(
         private authService: AuthService,
-        private _idle: Idle) {
+        private _idle: Idle,
+        public _toastr: ToastsManager,
+        vRef: ViewContainerRef) {
             _idle.setIdle(this._timeout);
             _idle.setTimeout(1);
             _idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
             _idle.onTimeout.subscribe(() => { this.logout(); });
             this.fullScreen = Boolean(localStorage.getItem("fullScreen"));
             this.reset();
+            this._toastr.setRootViewContainerRef(vRef);
     }
 
     ngOnInit() {
